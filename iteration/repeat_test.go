602208cc60d12d0057debcb8e21a21ecc8431024
ClemGamer <tsaiyouchen@gmail.com>
2025-01-19 21:47:40 +0800

    Add feature that caller of Repeat can specify how many times to repeat

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,15 +3,18 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	character := "a"
+	count := 5
+	repeated := Repeat(character, count)
 	expected := "aaaaa"
 	if repeated != expected {
-		t.Errorf("Expected %q, but got %q", expected, repeated)
+		t.Errorf("Expected %q when repeat %q by count %d, but got %q",
+			expected, character, count, repeated)
 	}
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
