b634d0e7a9c597045cc55ededdd9d41b4847eadc
ClemGamer <tsaiyouchen@gmail.com>
2025-01-21 00:05:26 +0800

    Add func SumAll and test and refact

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -13,5 +16,29 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safety sum empty slices", func(t *testing.T) {
+		got := SumAll([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
 	})
 }
