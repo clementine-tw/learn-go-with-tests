2343cef94e25128ca71eda35e69ccd6a7a20db2e
ClemGamer <tsaiyouchen@gmail.com>
2025-03-12 23:52:29 +0800

    Learned generics by implementing a stack

---
+++ generics/stack.go
@@ -0,0 +1,29 @@
+package generics
+
+type Stack[T any] struct {
+	values []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return new(Stack[T])
+}
+
+func (s *Stack[T]) Push(value T) {
+	s.values = append(s.values, value)
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if s.IsEmpty() {
+		var zero T
+		return zero, false
+	}
+
+	lastIndex := len(s.values) - 1
+	el := s.values[lastIndex]
+	s.values = s.values[:lastIndex]
+	return el, true
+}
